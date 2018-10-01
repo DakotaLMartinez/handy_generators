b647172b1419be11dade2751640c7218a4861456
DakotaLMartinez <dakotaleemusic@gmail.com>
2018-10-01 11:10:32 -0700

    add generators

---
+++ lib/generators/edit_links_helper_generator.rb
@@ -0,0 +1,28 @@
+class EditLinksHelperGenerator::EditLinksHelperGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+  class_option :pundit, type: :boolean, default: true
+
+  def generate_add
+    generate_edit_delete_links_helper
+    if options['pundit']
+      generate_edit_delete_links_partial
+    else
+      generate_edit_delete_links_partial_without_pundit
+    end
+  end
+
+  private 
+
+  def generate_edit_delete_links_helper
+    copy_file 'edit_delete_links_helper.template', 'app/helpers/edit_delete_links_helper.rb'
+  end
+
+  def generate_edit_delete_links_partial
+    copy_file 'edit_delete_links_partial.template', 'app/views/shared/_edit_delete_links.html.erb'
+  end
+
+  def generate_edit_delete_links_partial_without_pundit
+    copy_file 'edit_delete_links_partial_no_pundit.template', 'app/views/shared/_edit_delete_links.html.erb'
+  end
+end
+
