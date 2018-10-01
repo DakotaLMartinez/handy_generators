93782e432c79df289b8ee043a88a8295c2dcec1c
DakotaLMartinez <dakotaleemusic@gmail.com>
2018-10-01 16:52:30 -0700

    move into subdirectory

--- lib/handy_generators.rb
+++ lib/handy_generators.rb
@@ -2,5 +2,6 @@ require "handy_generators/version"
 
 module HandyGenerators
   require "rails/generators"
-  require_relative "./generators/edit_links_helper_generator"
+  require_relative "./edit_links_helper/generators/edit_links_helper_generator"
+  # require_relative "./generators/USAGE"
 end
